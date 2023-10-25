e812ca6f1cbd8e6b723744cf64b07a0482884d35
SAROJ SAH <sarojsaroj390@gmail.com>
2023-10-25 23:39:49 +0200

    hamburger menu added to navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.css";
 import brandLogo from "../../assets/brandLogo.png";
+import { TiThMenu } from "react-icons/ti";
+import { MdClose } from "react-icons/md";
 
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
   return (
     <div className={styles.header}>
       <div className="container">
         <div className={styles.header__contents}>
           <div className={styles.header__left}>
             <img className={styles.header__brandImage} src={brandLogo} alt="" />
-            {/* <h1 className={styles.header__brand}>
-              <span className={styles.only__w}>W</span>eb{" "}
-              <span className={styles.only__w}>W</span>izards
-            </h1> */}
           </div>
           <div className={styles.header__right}>
-            <ul className={styles.nav__links}>
+            <ul
+              className={`${styles.nav__links} ${isNavOpen ? styles.open : ""}`}
+            >
               <li className={styles.nav__link}>
                 <a href="/">Home</a>
               </li>
@@ -29,6 +34,12 @@ const Header = () => {
                 <a href="#contact">Contact</a>
               </li>
             </ul>
+            <li
+              className={`${styles.menuIcon} ${styles.nav__link}`}
+              onClick={toggleNav}
+            >
+              {!isNavOpen ? <TiThMenu /> : <MdClose />}
+            </li>
           </div>
         </div>
       </div>
