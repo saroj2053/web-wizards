f524fc4d0e9b180b6bce81d1c717f2ba0039bb0f
SAROJ SAH <sarojsaroj390@gmail.com>
2023-10-25 17:43:31 +0200

    changes to about paragraph

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,16 +10,12 @@ const About = () => {
         <div className={aboutStyles.about__contents}>
           <div className={aboutStyles.about__right}>
             <p>
-              Located in the heart of Chemnitz, Web Wizards is a
-              technology-driven company dedicated to providing cutting-edge
-              solutions for our valued customers. With a commitment to
-              excellence, we promise to harness the power of the latest tech
-              tools to solve your most pressing problems. Our experienced team's
-              passion for innovation, customer-centric approach, and unwavering
-              reliability makes us your trusted partner in navigating the
-              ever-evolving technological landscape. Join us in our mission to
-              empower you with the best technology, tailored to your needs, and
-              embark on a journey of progress and success.
+              Web Wizards in Chemnitz offers cutting-edge tech solutions, driven
+              by our passionate team's commitment to excellence. We're your
+              reliable partner in navigating evolving tech landscapes, tailoring
+              solutions to empower your progress and success. Join us in our
+              mission to empower you with the best technology, tailored to your
+              needs, and embark on a journey of progress and success.
             </p>
           </div>
         </div>
