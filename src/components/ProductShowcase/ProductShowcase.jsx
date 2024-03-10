908eac111eddc2eabbb157ce0fddff13521e398e
Saroj Sah <saroj.sah@s2022.tu-chemnitz.de>
2024-03-10 19:20:21 +0100

    modify certain contents under product highlights section

--- src/components/ProductShowcase/ProductShowcase.jsx
+++ src/components/ProductShowcase/ProductShowcase.jsx
@@ -9,7 +9,10 @@ import productLaunchImg from "../../assets/campusflow-launch.png";
 
 const ProductShowcase = () => {
   const openLink = () => {
-    window.open("http://campusflowfrontend.s3-website.eu-north-1.amazonaws.com/", "_blank");
+    window.open(
+      "http://campusflowfrontend.s3-website.eu-north-1.amazonaws.com/",
+      "_blank"
+    );
   };
 
   return (
@@ -17,40 +20,6 @@ const ProductShowcase = () => {
       <div className="container">
         <h1 className={styles.productShowcase__heading}>Our Products</h1>
         <div className={styles.productCard__wrapper}>
-          {/* <div className={styles.productCard}>
-            <figure className={styles.productImage}>
-              <img
-                src={productThumbnail}
-                alt="Across platform, under development"
-              />
-
-              <figcaption>
-                <main>
-                  <p className={styles.small}>Under Development...</p>
-                  <h3>
-                    Campus Flow, <em>An across Web Application</em>
-                  </h3>
-                  <p>
-                    The CampusFlow by Web Wızards provides a variety of
-                    functions for students to organise and administer their
-                    across courses.
-                  </p>
-                </main>
-              </figcaption>
-            </figure>
-          </div>
-
-          <p className={aboutStyles.padding_mobile}>
-            Welcome to CampusFlow, where we redefine your university experience
-            with a unified system that puts all the essential tools at your
-            fingertips. Navigate the vast landscape of university modules
-            effortlessly, explore seamless credit transfer options, and make
-            informed decisions through our intuitive module comparison feature.
-            CampusFlow is more than a product – it's your passport to a
-            smoother, more informed academic journey. Join us and unlock the
-            potential of streamlined university living today!
-          </p> */}
-
           <Carousel autoPlay>
             <div>
               <img className="carousel__img" src={productLaunchImg} alt="" />
@@ -58,7 +27,8 @@ const ProductShowcase = () => {
                 className="legend"
                 style={{ fontSize: "18px", fontWeight: "400" }}
               >
-                Our Product is Live now, you can take an experience by visiting Go Live link below
+                Our Product is Live now, you can take an experience by visiting
+                Go Live link below
               </h2>
             </div>
           </Carousel>
--- src/components/ProjectCarousel/ProjectCarousel.jsx
+++ src/components/ProjectCarousel/ProjectCarousel.jsx
@@ -26,131 +26,135 @@ import giphyImg from "../../assets/giphy.gif";
 const ProjectCarousel = () => (
   <>
     <div className="product__highlights">
-      {/* <h1 className="product__highlights-heading">Product Highlights</h1>
-      <p>Product Launch will commence on 5th March at 12PM...</p>
+      <h1 className="product__highlights-heading">Product Highlights</h1>
+      {/* <p>Product Launch will commence on 5th March at 12PM...</p>
       <img
         style={{ width: "30%", objectFit: "contain", margin: "0 auto" }}
         src={giphyImg}
         alt="minion playing guitar"
       /> */}
-    </div>
 
-    <Carousel autoPlay>
-      <div>
-        <img className="carousel__img" alt="" src={index1Img} />
-        <p className="legend" id="format">
-          <b>User landing Page</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index2Img} />
-        <p className="legend" id="format">
-          <b>List of modules across all universities</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index3Img} />
-        <p className="legend" id="format">
-          <b>List of Across Universities</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index4Img} />
-        <p className="legend" id="format">:partying_face:
-          <b>Users can mark their completed modules</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index5Img} />
-        <p className="legend" id="format">
-          <b>User Profile Section</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index6Img} />
-        <p className="legend" id="format">
-          <b>Here you can find comparable modules against your respective module from another university</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index7Img} />
-        <p className="legend" id="format">
-          <b>Transfer Credits Declaration Page</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index8Img} />
-        <p className="legend" id="format">
-          <b>Choose your university where you want to transfer your credits</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index9Img} />
-        <p className="legend" id="format">
-          <b>Select from which completed modules you want to ask for transfer credits</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index10Img} />
-        <p className="legend" id="format">
-          <b>Upload your transcript and get verified</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index11Img} />
-        <p className="legend" id="format">
-          <b>See your transfer credit modules option in another university</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index12Img} />
-        <p className="legend" id="format">
-          <b>Upload your signature</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index13Img} />
-        <p className="legend" id="format">
-          <b>Now, you're done with transfer credits process, wait for one week to get confirmation from university</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index19Img} />
-        <p className="legend" id="format">
-          <b>User will get notified through email and notification section on their homepage</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index14Img} />
-        <p className="legend" id="format">
-          <b>Admin Home Page</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index15Img} />
-        <p className="legend" id="format">
-          <b>Admin Panel - User Management</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index16Img} />
-        <p className="legend" id="format">
-          <b>Admin Panel - Similarity Table for which you have uploaded the pdf</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index17Img} />
-        <p className="legend" id="format">
-          <b>Admin Panel - One-Click PDF Upload</b>
-        </p>
-      </div>
-      <div>
-        <img className="carousel__img" alt="" src={index18Img} />
-        <p className="legend" id="format">
-          <b>Admin Panel - Admin can manage users transfer credit requests</b>
-        </p>
-      </div>
-    </Carousel>
+      <Carousel autoPlay>
+        <div>
+          <img className="carousel__img" alt="" src={index1Img} />
+          <p className="legend" id="format">
+            User landing Page
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index2Img} />
+          <p className="legend" id="format">
+            List of modules across all universities
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index3Img} />
+          <p className="legend" id="format">
+            List of Across Universities
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index4Img} />
+          <p className="legend" id="format">
+            Users can mark their completed modules
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index5Img} />
+          <p className="legend" id="format">
+            User Profile Section
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index6Img} />
+          <p className="legend" id="format">
+            Here you can find comparable modules against your respective module
+            from another university
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index7Img} />
+          <p className="legend" id="format">
+            Transfer Credits Declaration Page
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index8Img} />
+          <p className="legend" id="format">
+            Choose your university where you want to transfer your credits
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index9Img} />
+          <p className="legend" id="format">
+            Select from which completed modules you want to ask for transfer
+            credits
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index10Img} />
+          <p className="legend" id="format">
+            Upload your transcript and get verified
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index11Img} />
+          <p className="legend" id="format">
+            See your transfer credit modules option in another university
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index12Img} />
+          <p className="legend" id="format">
+            Upload your signature
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index13Img} />
+          <p className="legend" id="format">
+            Now, you're done with transfer credits process, wait for one week to
+            get confirmation from university
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index19Img} />
+          <p className="legend" id="format">
+            User will get notified through email and notification section on
+            their homepage
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index14Img} />
+          <p className="legend" id="format">
+            Admin Home Page
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index15Img} />
+          <p className="legend" id="format">
+            Admin Panel - User Management
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index16Img} />
+          <p className="legend" id="format">
+            Admin Panel - Similarity Table for which you have uploaded the pdf
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index17Img} />
+          <p className="legend" id="format">
+            Admin Panel - One-Click PDF Upload
+          </p>
+        </div>
+        <div>
+          <img className="carousel__img" alt="" src={index18Img} />
+          <p className="legend" id="format">
+            Admin Panel - Admin can manage users transfer credit requests
+          </p>
+        </div>
+      </Carousel>
+    </div>
   </>
 );
 
